7c518ca34abc9f825f0ffeee13da90247980d66b
Michael Tryby <tryby.michael@epa.gov>
2020-04-06 17:30:07 -0400

    Work in progress
    Fixes segfult when loading epanet.toolkit package

--- epanet-toolkit/setup.py
+++ epanet-toolkit/setup.py
@@ -8,7 +8,8 @@
 #             US EPA - ORD/NRMRL
 #
 # Suggested Usage:
-#   python setup.py build -- -G"Visual Studio 15 2017 Win64" ..
+#   python setup.py build
+#   python setup.py bdist_wheel
 #
 
 
@@ -33,18 +34,22 @@ else:
 
 
 setup(
-    name = 'epanet-toolkit',
-    version = '0.5.0',
+    name = "epanet-toolkit",
+    version = "0.5.0",
 
 #    description='',
 #    long_description='',
 
     cmake_args = cmake_args,
 
-    package_dir = {'': 'src'},
-    packages = ['epanet.toolkit'],
+    package_dir = {"": "src"},
+    packages = ["epanet.toolkit"],
 
     include_package_data = True,
 
-    zip_safe = False
+    zip_safe = False,
+
+    install_requires=[
+        "aenum"
+    ]
 )
