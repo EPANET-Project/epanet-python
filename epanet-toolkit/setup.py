703a7950dfafcd181d9cfb792d821af0db277fd1
Michael Tryby <tryby.michael@epa.gov>
2020-05-04 15:17:55 -0400

    Cleaning up

--- epanet-toolkit/setup.py
+++ epanet-toolkit/setup.py
@@ -57,18 +57,15 @@ if platform_system == "Windows":
     cmake_args = ["-GVisual Studio 14 2015 Win64"]
 
 elif platform_system == "Darwin":
-    cmake_args = ["-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.10"] #python v3.7
+    cmake_args = ["-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.10"]
 
 else:
-    cmake_args = ["-Unix Makefiles"]
+    cmake_args = ["-GUnix Makefiles"]
 
 
 setup(
     name = "epanet-toolkit",
     version = "0.5.0",
-
-#    description='',
-#    long_description='',
 
     cmake_args = cmake_args,
 
