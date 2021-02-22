ee406e4e8fe818f8a01b6daf3c25ba432ca3f7bf
Michael Tryby <tryby.michael@epa.gov>
2021-02-22 09:19:09 -0500

    Updating Visual Studio

--- epanet-toolkit/setup.py
+++ epanet-toolkit/setup.py
@@ -58,7 +58,7 @@ class CleanCommand(Command):
 
 # Set platform specific cmake args here
 if platform_system == "Windows":
-    cmake_args = ["-GVisual Studio 14 2015 Win64"]
+    cmake_args = ["-GVisual Studio 15 2017 Win64"]
 
 elif platform_system == "Darwin":
     cmake_args = ["-GXcode", "-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.10"]
